a22f706efc9cb3c51bf2e89e4bb5a2966121b9db
SomeRaven <d00439951@utahtech.edu>
2025-05-20 11:19:46 -0600

    fix 5

--- public/signin-signout.js
+++ public/signin-signout.js
@@ -32,8 +32,6 @@ createApp({
         
             // 👇 Log all non-HttpOnly cookies (this won't show session cookie)
             console.log("🍪 document.cookie:", document.cookie);
-            console.log("Session after login:", req.session);
-
         
             // 👇 You can still see session cookie in browser DevTools > Application > Cookies
         
